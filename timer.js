a39c104b0b0485132258432c10d17a4fbb51b77b
Brian-dev88 <24255@burnside.school.nz>
2026-08-19 13:44:51 +1200

    Timer completed and added banner for introduction

---
+++ timer.js
@@ -0,0 +1,27 @@
+const Days = document.getElementById('days');
+const Hours = document.getElementById('hours');
+const Minutes = document.getElementById('minutes');
+const Seconds = document.getElementById('seconds');
+
+const targetDate = new Date('July 30 2026 07:00:00').getTime();
+
+function timer () {
+    const currentDate = new Date().getTime();
+    const distance = targetDate - currentDate;
+
+    const days =  Math.abs(Math.floor(distance / 1000 / 60 /60 / 24));
+    const hours = Math.abs(Math.floor(distance / 1000 / 60 / 60) % 24);
+    const minutes = Math.abs(Math.floor(distance / 1000 / 60) % 60);
+    const seconds = Math.abs(Math.floor(distance / 1000) % 60);
+
+    console.log(days + ':' + hours + ':' + minutes + ':' + seconds);
+
+    Days.innerHTML = days;
+    Hours.innerHTML = hours;
+    Minutes.innerHTML = minutes;
+    Seconds.innerHTML = seconds;
+
+
+}
+
+setInterval(timer, 1000);
